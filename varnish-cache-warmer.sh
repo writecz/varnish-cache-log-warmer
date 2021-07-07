55a00fbd6de53786e8fa2309949262329aff550b
Write.cz <jan@write.cz>
2021-07-07 23:56:47 +0200

    Update varnish-cache-warmer.sh

--- varnish-cache-warmer.sh
+++ varnish-cache-warmer.sh
@@ -1,31 +1,69 @@
 #!/bin/bash
 #
-# Written by AC - 2015 <contact@chemaly.com> - sys0dm1n.com
+# Originally written by AC - 2015 <contact@chemaly.com> - sys0dm1n.com
+# process run check, load detection and varnishncsa as data source by Write.cz - 2021 (dev@write.cz)
 #
-# The URLs or IPs variables are passed as an argument to the script like below:
-# ./varnish-cache-warmer.sh example1.com example2.com example3.com
 
-VERSION='1.0.0'
-USER_AGENT="VarnishCacheWarmer/$VERSION (see https://github.com/sys0dm1n/varnish-cache-warmer)"
-
-warm_varnish() {
-    echo "Warming cache for $1"
-    curl -sL -A "$USER_AGENT" http://$1/sitemap.xml | egrep -o "http(s?)://$1[^ \"\'()\<>]+" | while read line; do
-        if [[ $line == *.xml ]]
-        then
-            newURL=$line
-            curl -sL -A "$USER_AGENT" $newURL | egrep -o "http(s?)://$1[^ \"\'()\<>]+" | while read newline; do
-                time curl -sL -A "$USER_AGENT" -sL -w "%{http_code} %{url_effective}\n" $newline -o /dev/null 2>&1
-                echo $newline
-            done
-        else
-            time curl -sL -A "$USER_AGENT" -sL -w "%{http_code} %{url_effective}\n" $line -o /dev/null 2>&1
-            echo $line
-        fi
-    done
-    echo "Done warming cache for $1"
+# check if script is already running
+getscript() {
+  pgrep -lf "varnish-cache-warmer" | wc -l
 }
 
-for host in "$@"; do
-    warm_varnish $host
-done
+RUNNING=`getscript`
+
+VERSION='1.0.1'
+
+echo $RUNNING
+
+# Allow only one instance running
+
+if (($RUNNING > 2)); then
+        getscript |  wc -l
+        echo "Script already running"
+        exit 0;
+fi
+
+NOTIFY="3"
+
+TRUE="1"
+
+warm_varnish() {
+            # get unique URLS from varnisncsa logs and warm 'em up
+            awk {'print $7'} /var/log/varnish/* | grep "\.html" | sort | uniq -c | sort -nr | awk {'print $2'} | while read newline; do
+                time curl -sL $newline \
+                  -H 'authority: luxusnioznameni.cz' \
+                  -H 'accept-encoding: gzip, deflate, br' \
+                  -H 'dnt: 1' \
+                  -H 'upgrade-insecure-requests: 1' \
+                  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 OPR/69.0.3686.49 VCW/1.0.1' \
+                  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+                  -H 'sec-fetch-site: none' \
+                  -H 'sec-fetch-mode: navigate' \
+                  -H 'sec-fetch-user: ?1' \
+                  -H 'sec-fetch-dest: document' \
+                  -H 'accept-language: cs-CZ,cs;q=0.9' \
+                  --compressed \
+                  --cookie-jar /tmp/cookie.txt \
+                  -o /dev/null 2>&1
+                echo $newline
+                
+                # check 5 minute system load and pause script if load is high
+
+                LOAD5MIN="$(uptime | awk -F 'load average:' '{ print $2 }' | cut -d, -f2 | sed 's/ //g')"
+
+                RESULT=$(echo "$LOAD5MIN > $NOTIFY" | bc)
+
+                if [ "$RESULT" == "$TRUE" ]; then
+                        sleep 5m;
+                fi
+
+                # stop the script when Varnish cache is purged
+
+                PURGE=`tail -n 50 /var/log/varnish/varnishncsa.log | grep PURGE`
+                if [ "$PURGE" ]; then
+                        exit 0;
+                fi
+            done
+}
+
+warm_varnish
